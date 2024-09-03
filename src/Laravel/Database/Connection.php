d226b0bac105af272a58baf9a57fe8221ca3be8f
Sasaya <storyn26383@gmail.com>
2024-09-03 23:53:33 +0800

    feat: connection

---
+++ src/Laravel/Database/Connection.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace SwooleTW\ClickHouse\Laravel\Database;
+
+use ClickHouseDB\Client;
+use ClickHouseDB\Quote\ValueFormatter;
+use Exception;
+use Illuminate\Database\Connection as BaseConnection;
+use SwooleTW\ClickHouse\Laravel\Database\Query\Grammar;
+
+class Connection extends BaseConnection
+{
+    /**
+     * The ClickHouse client.
+     */
+    protected Client $client;
+
+    /**
+     * Create a new database connection instance.
+     *
+     * @param  array<string, mixed>  $config
+     */
+    public function __construct(string $database = '', string $tablePrefix = '', array $config = [], ?Client $client = null)
+    {
+        $this->database = $database;
+        $this->tablePrefix = $tablePrefix;
+        $this->config = $config;
+        $this->client = $client ?? $this->getDefaultClient($database, $config);
+
+        $this->useDefaultQueryGrammar();
+        $this->useDefaultPostProcessor();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param  mixed[]  $bindings
+     * @return array<string, mixed>[]
+     */
+    public function select($query, $bindings = [], $useReadPdo = true): array
+    {
+        // @phpstan-ignore-next-line
+        return $this->run($query, $bindings, function (string $query, array $bindings) {
+            $sql = $this->toRawSql($query, $bindings);
+
+            $statement = $this->client->select($sql);
+
+            return $statement->rows();
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param  mixed[]  $bindings
+     */
+    public function statement($query, $bindings = []): bool
+    {
+        // @phpstan-ignore-next-line
+        return $this->run($query, $bindings, function ($query, $bindings) {
+            $sql = $this->toRawSql($query, $bindings);
+
+            $this->client->write($sql);
+
+            return true;
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param  mixed[]  $bindings
+     */
+    public function affectingStatement($query, $bindings = []): int
+    {
+        // @phpstan-ignore-next-line
+        return $this->run($query, $bindings, function ($query, $bindings) {
+            $sql = $this->toRawSql($query, $bindings);
+
+            $this->client->write($sql);
+
+            // TODO: correct affected rows
+            return 1;
+        });
+    }
+
+    /** {@inheritDoc} */
+    public function escape($value, $binary = false): string
+    {
+        // @phpstan-ignore-next-line
+        return (string) ValueFormatter::formatValue($value);
+    }
+
+    /** {@inheritDoc} */
+    public function reconnectIfMissingConnection() {}
+
+    /** {@inheritDoc} */
+    public function disconnect() {}
+
+    /** {@inheritDoc} */
+    public function getSchemaBuilder()
+    {
+        // TODO: implement schema builder
+        throw new Exception('Not supported yet');
+    }
+
+    /** {@inheritDoc} */
+    protected function getDefaultQueryGrammar()
+    {
+        return (new Grammar)->setConnection($this);
+    }
+
+    /** {@inheritDoc} */
+    protected function getDefaultSchemaGrammar()
+    {
+        // TODO: implement schema builder
+        throw new Exception('Not supported yet');
+    }
+
+    /**
+     * Get the default ClickHouse client.
+     *
+     * @param  array<string, mixed>  $config
+     */
+    protected function getDefaultClient(string $database, array $config): Client
+    {
+        $client = new Client($config);
+
+        if ($database) {
+            $client->database($database);
+        }
+
+        return $client;
+    }
+
+    /**
+     * Get the raw SQL representation with bindings.
+     *
+     * @param  mixed[]  $bindings
+     */
+    protected function toRawSql(string $query, array $bindings): string
+    {
+        return $this->queryGrammar->substituteBindingsIntoRawSql(
+            $query,
+            $this->prepareBindings($bindings)
+        );
+    }
+}
---
+++ tests/Laravel/Database/ConnectionTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SwooleTW\ClickHouse\Tests\Laravel\Database;
+
+use ClickHouseDB\Client;
+use ClickHouseDB\Statement;
+use SwooleTW\ClickHouse\Laravel\Database\Connection;
+use SwooleTW\ClickHouse\Tests\TestCase;
+
+class ConnectionTest extends TestCase
+{
+    public function testSelect()
+    {
+        $expected = [['column' => 'value']];
+
+        $client = $this->mock(Client::class);
+        $statement = $this->mock(Statement::class);
+        $connection = new Connection(client: $client);
+
+        $client->shouldReceive('select')->with("select * from `table` where `column` = 'value'")->once()->andReturn($statement);
+        $statement->shouldReceive('rows')->withNoArgs()->once()->andReturn($expected);
+
+        $actual = $connection->select('select * from `table` where `column` = ?', ['value']);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testInsert()
+    {
+        $client = $this->mock(Client::class);
+        $connection = new Connection(client: $client);
+
+        $client->shouldReceive('write')->with("insert into `table` (`column`) values ('value')")->once();
+
+        $actual = $connection->insert('insert into `table` (`column`) values (?)', ['value']);
+
+        $this->assertTrue($actual);
+    }
+
+    public function testUpdate()
+    {
+        $client = $this->mock(Client::class);
+        $connection = new Connection(client: $client);
+
+        $client->shouldReceive('write')->with("alter table `table` update `column` = 'value_b' where `column` = 'value_a'")->once();
+
+        $actual = $connection->update('alter table `table` update `column` = ? where `column` = ?', ['value_b', 'value_a']);
+
+        // TODO: correct affected rows
+        $this->assertEquals(1, $actual);
+    }
+
+    public function testDelete()
+    {
+        $client = $this->mock(Client::class);
+        $connection = new Connection(client: $client);
+
+        $client->shouldReceive('write')->with("alter table `table` delete where `column` = 'value'")->once();
+
+        $actual = $connection->delete('alter table `table` delete where `column` = ?', ['value']);
+
+        // TODO: correct affected rows
+        $this->assertEquals(1, $actual);
+    }
+}
