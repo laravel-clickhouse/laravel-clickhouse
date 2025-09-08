cb5003ac2d4c40c8103e414c8818f76668629b43
Sasaya <storyn26383@gmail.com>
2025-09-08 14:38:13 +0800

    feat: add custom migration repository for ClickHouse database support

--- src/Laravel/ClickHouseServiceProvider.php
+++ src/Laravel/ClickHouseServiceProvider.php
@@ -3,6 +3,8 @@
 namespace ClickHouse\Laravel;
 
 use ClickHouse\Laravel\Eloquent\Model;
+use ClickHouse\Laravel\Migrations\DatabaseMigrationRepository;
+use Illuminate\Database\Console\Migrations\InstallCommand as MigrateInstallCommand;
 use Illuminate\Support\ServiceProvider;
 
 class ClickHouseServiceProvider extends ServiceProvider
@@ -30,6 +32,16 @@ class ClickHouseServiceProvider extends ServiceProvider
                     database: $config['database'] ?? '',
                     config: array_merge($config, compact('name'))
                 );
+            });
+        });
+
+        $this->app->beforeResolving(MigrateInstallCommand::class, function () {
+            $this->app->singleton('migration.repository', function ($app) {
+                $migrations = $app['config']['database.migrations'];
+
+                $table = is_array($migrations) ? ($migrations['table'] ?? null) : $migrations;
+
+                return new DatabaseMigrationRepository($app['db'], $table);
             });
         });
     }
---
+++ src/Laravel/Migrations/DatabaseMigrationRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ClickHouse\Laravel\Migrations;
+
+use ClickHouse\Laravel\Schema\Builder as SchemaBuilder;
+use Illuminate\Database\Migrations\DatabaseMigrationRepository as BaseDatabaseMigrationRepository;
+
+class DatabaseMigrationRepository extends BaseDatabaseMigrationRepository
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function createRepository()
+    {
+        $schema = $this->getConnection()->getSchemaBuilder();
+
+        if (! $schema instanceof SchemaBuilder) {
+            parent::createRepository();
+
+            return;
+        }
+
+        $schema->create($this->table, function ($table) {
+            $table->text('migration');
+            $table->integer('batch');
+        });
+    }
+}
