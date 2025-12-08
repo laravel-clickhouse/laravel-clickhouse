783d4740421ccda80444a5bfb79c9da880547518
Sasaya <storyn26383@gmail.com>
2025-12-08 21:20:57 +0800

    feat: enhance dx

--- src/Laravel/Eloquent/Model.php
+++ src/Laravel/Eloquent/Model.php
@@ -2,8 +2,21 @@
 
 namespace ClickHouse\Laravel\Eloquent;
 
+use ClickHouse\Laravel\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Database\Eloquent\Scope;
 
+/**
+ * @method static Builder<static> query()
+ * @method Builder<static> newQuery()
+ * @method Builder<static> newModelQuery()
+ * @method Builder<static> newQueryWithoutRelationships()
+ * @method Builder<static> newQueryWithoutScopes()
+ * @method Builder<static> newQueryWithoutScope(Scope|string $scope)
+ * @method Builder<static> newQueryForRestoration(array|int $ids)
+ * @method Builder<*> newEloquentBuilder(QueryBuilder $query)
+ * @method QueryBuilder newBaseQueryBuilder()
+ */
 abstract class Model extends BaseModel
 {
     /** {@inheritDoc} */
